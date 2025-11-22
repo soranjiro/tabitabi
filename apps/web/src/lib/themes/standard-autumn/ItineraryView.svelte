3a68aeec89dab42e81a8987ed0d7a1e334c6616b
soranjiro <yutayuta540@gmail.com>
2025-11-22 18:26:26 +0900

    feat(web): add share link buttons with updated styles

--- apps/web/src/lib/themes/standard-autumn/ItineraryView.svelte
+++ apps/web/src/lib/themes/standard-autumn/ItineraryView.svelte
@@ -685,12 +685,48 @@
           どのリンクをコピーしますか?
         </p>
         <div class="standard-autumn-share-options">
+          <button
+            onclick={() => copyShareLink(false)}
+            class="standard-autumn-share-option"
+          >
+            <div class="standard-autumn-share-option-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                width="24"
+                height="24"
+              >
+                <path
+                  d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"
+                />
+              </svg>
+            </div>
+            <div class="standard-autumn-share-option-content">
+              <div class="standard-autumn-share-option-title">閲覧用リンク</div>
+              <div class="standard-autumn-share-option-desc">
+                閲覧のみ可能なリンクをコピー
+              </div>
+            </div>
+          </button>
           {#if hasEditPermission}
             <button
               onclick={() => copyShareLink(true)}
               class="standard-autumn-share-option"
             >
-              <div class="standard-autumn-share-option-icon">🔓</div>
+              <div class="standard-autumn-share-option-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  width="24"
+                  height="24"
+                >
+                  <path
+                    d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"
+                  />
+                </svg>
+              </div>
               <div class="standard-autumn-share-option-content">
                 <div class="standard-autumn-share-option-title">
                   編集用リンク
@@ -701,18 +737,6 @@
               </div>
             </button>
           {/if}
-          <button
-            onclick={() => copyShareLink(false)}
-            class="standard-autumn-share-option"
-          >
-            <div class="standard-autumn-share-option-icon">👁️</div>
-            <div class="standard-autumn-share-option-content">
-              <div class="standard-autumn-share-option-title">閲覧用リンク</div>
-              <div class="standard-autumn-share-option-desc">
-                閲覧のみ可能なリンクをコピー
-              </div>
-            </div>
-          </button>
         </div>
         <button
           onclick={() => {

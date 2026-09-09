9d9a52e65b58bf7b54e59428882226180ddfa4d6
soranjiro <122503868+soranjiro@users.noreply.github.com>
2026-09-10 07:25:01 +0900

    perf(web): scope legacy itinerary background CSS

--- apps/web/src/routes/[id]/+layout.svelte
+++ apps/web/src/routes/[id]/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import "$lib/itinerary/background-cover.css";
   import ExternalSiteOverlay from "$lib/external-site-overlay/ExternalSiteOverlay.svelte";
   import BackgroundControls from "$lib/itinerary/BackgroundControls.svelte";
 </script>
